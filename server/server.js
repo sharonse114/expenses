d88409ac9a66ce4666e8a218fed06629a127a633
Sharon <sharon@Kaseys-MacBook-Air.local>
2018-09-08 12:19:12 -0700

    MongodbPassWordUpdate

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
-mongoose.connect('mongodb://<sharonse114>:<Trebor915!>@ds001.mlab.com:001/db101');
+mongoose.connect('mongodb://sharonse:abc123@ds249942.mlab.com:49942/expenses');
+//mongoose.connect('mongodb://$sharonse114:$Trebor915!@${uri}/${db}?authMechanism=SCRAM-SHA-1')
 app.use('/', router);
 module.exports=app;
